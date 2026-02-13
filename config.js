7faf5a650a4388147631c1e3fe39a09140be353d
bloodrush30 <90501911+bloodrush30@users.noreply.github.com>
2026-02-13 17:26:18 +0800

    config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dtj0nnohy/video/upload/v1770974633/fitterkarma_-_Pag-Ibig_ay_Kanibalismo_II__mp3.pm_u3ikpk.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
